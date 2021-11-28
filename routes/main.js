e576355a104ff4e0e8263ed5ed196e72e075c08a
nandeep21 <nandeep@openxcellinc.com>
2021-11-28 11:49:29 +0530

    Basic setup

---
+++ routes/main.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const router = express.Router()
+
+router.get('/', (req, res, next) => {
+    res.render('home',req.context)
+})
+
+router.get('/blog', (req, res, next) => {
+    res.render('blog',req.context)
+})
+
+module.exports = router
